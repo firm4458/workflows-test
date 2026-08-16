27fe5ebdd4e7d18f95307257c13b4c21b80f6939
Pattanachai Chonglertvarawong <pattanachai_chonglertvarawong@pj100.biz>
2026-08-17 07:49:00 +0900

    Check for warning suppressions

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -27,4 +27,25 @@ public class DemoApplication {
 		}
 	} // test
 
+
+	// Because I'm too lazy to fix this now
+	@SuppressWarnings("PMD.CognitiveComplexity")
+	public static void foo() {
+		for (int i=0; i<5; ++i){
+			for (int j=0; j<5; ++j){
+				for (int k=0; k<5; ++k) {
+					System.out.println("AAAAA");
+				}
+			}
+		}
+
+		for (int i=0; i<5; ++i){
+			for (int j=0; j<5; ++j){
+				for (int k=0; k<5; ++k) {
+					if (k%2==0) System.out.println("AAAAA");
+					else  System.out.println("BBBBBB");
+				}
+			}
+		}
+	}
 }
